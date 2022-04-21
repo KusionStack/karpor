9c3c6fdf8bd079ae7eb5cb9ed20a03acc407d3a6
方寸 <fangcun.zw@antfin.com>
2022-04-21 19:20:19 +0800

    new api: allow to fetch pod lists
    Add new api for testing the interaction with K8s, the new api is used to
    fetch pod lists under some specified namespace.
    
    Usage from client:
    
    ```
    curl -XGET http://localhost:8080/ns/kube-system/pods
    ```
    
    The result will be something like:
    
    ```
    {"pods":["kindnet-lgn5n","kube-apiserver-kind-control-plane",
    "etcd-kind-control-plane","kube-controller-manager-kind-control-plane",
    "kube-proxy-b94h8","coredns-64897985d-nnm8x","coredns-64897985d-nx694",
    "kube-scheduler-kind-control-plane"]}
    ```
    
    Signed-off-by: 方寸 <fangcun.zw@antfin.com>

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,12 +1,35 @@
 package handler
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	listerv1 "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
-var db = make(map[string]string)
+var (
+	db        = make(map[string]string)
+	podlister listerv1.PodLister
+)
+
+func init() {
+	var err error
+	podlister, err = newPodLister()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func PingHandler(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
@@ -41,4 +64,65 @@ func AdminHandler(c *gin.Context) {
 		db[user] = json.Value
 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
 	}
+}
+
+func GetPods(c *gin.Context) {
+	ns := c.Params.ByName("ns")
+	pods, err := podlister.Pods(ns).List(labels.Set{}.AsSelector())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// We only want pod names.
+	var podNames []string
+	for _, pod := range pods {
+		podNames = append(podNames, pod.Name)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"pods": podNames})
+}
+
+func newPodLister() (listerv1.PodLister, error) {
+	defaultKubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	if _, err := os.Stat(defaultKubeconfig); err != nil {
+		defaultKubeconfig = ""
+	}
+	log.Printf("using kubeconfig: %s", defaultKubeconfig)
+
+	// Create a kubernetes config.
+	config, err := clientcmd.BuildConfigFromFlags("", defaultKubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("get cluster K8s config failed: %v", err)
+	}
+
+	// Create the clientset.
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("make new k8s clientset failed: %v", err)
+	}
+
+	// Create informer.
+	kubeInformers := informers.NewSharedInformerFactoryWithOptions(
+		clientset, 5*time.Minute,
+		informers.WithTweakListOptions(func(options *metav1.ListOptions) {
+		}),
+	)
+
+	podInformer := kubeInformers.Core().V1().Pods()
+
+	// Register pod informer
+	inf := podInformer.Informer()
+
+	log.Println("start pod informers")
+	stopCh := make(chan struct{})
+	kubeInformers.Start(stopCh)
+
+	log.Println("wait for pod informer cache sync ready...")
+	if !cache.WaitForCacheSync(stopCh, inf.HasSynced) {
+		return nil, fmt.Errorf("failed to sync K8s pod cache")
+	}
+
+	log.Println("pod informer cache is synced done")
+	return podInformer.Lister(), nil
 }
