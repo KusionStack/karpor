353da624476db6ad739dbf14d140bc66bdde0fd5
panshuai-ps <pjjwjw@163.com>
2023-04-20 19:54:38 +0800

    fix: import error

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./app";
 // import reportWebVitals from "./reportWebVitals";
 import "./utils/request";
 
