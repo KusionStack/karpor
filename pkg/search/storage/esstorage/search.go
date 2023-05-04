77588011479fc0d9809a9c102ca67bd8b86a7fdf
panshuai-ps <pjjwjw@163.com>
2023-05-04 11:56:34 +0800

    fix: lint and license

--- pkg/search/storage/esstorage/search.go
+++ pkg/search/storage/esstorage/search.go
@@ -1,3 +1,17 @@
+// Copyright The Karbour Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package esstorage
 
 import (
@@ -14,9 +28,8 @@ func (s *ESClient) Search(ctx context.Context, queries []storage.Query) (*storag
 		for _, v := range query.Values {
 			values = append(values, v)
 		}
-		switch query.Operator {
 		// TODO: support other operators
-		case storage.Equals:
+		if query.Operator == storage.Equals {
 			boolQuery.Must(esquery.Terms(query.Key, values...))
 		}
 	}
