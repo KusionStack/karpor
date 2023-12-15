d2db8b38a0e698d4ebe8e65d62d3ee8f6219ef54
Forest <forest10161016@gmail.com>
2023-12-15 15:58:28 +0800

    fix: fixed an issue with building cluster clients (#124)
    ## What type of PR is this?
    
    /kind bug
    
    ## What this PR does / why we need it:
    
    Karbour needs unmasked cluster credentials to build spoke clients

--- pkg/registry/cluster/storage.go
+++ pkg/registry/cluster/storage.go
@@ -17,15 +17,8 @@ limitations under the License.
 package cluster
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/KusionStack/karbour/pkg/apis/cluster"
-	clustermgr "github.com/KusionStack/karbour/pkg/core/manager/cluster"
 
-	"k8s.io/apimachinery/pkg/apis/meta/internalversion"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/registry/generic"
 	genericregistry "k8s.io/apiserver/pkg/registry/generic/registry"
@@ -81,45 +74,4 @@ type REST struct {
 // ShortNames implements the ShortNamesProvider interface. Returns a list of short names for a resource.
 func (r *REST) ShortNames() []string {
 	return []string{"cl"}
-}
-
-// Get retrieves the object from the storage. It is required to support Patch.
-func (r *REST) List(ctx context.Context, options *internalversion.ListOptions) (runtime.Object, error) {
-	sanitizedClusterList := &cluster.ClusterList{}
-	clusterList, err := r.Store.List(ctx, options)
-	if err != nil {
-		return sanitizedClusterList, err
-	}
-	for _, c := range clusterList.(*cluster.ClusterList).Items {
-		// Convert to unstructured
-		clusterMap, _ := runtime.DefaultUnstructuredConverter.ToUnstructured(&c)
-		clusterUnstructured := &unstructured.Unstructured{}
-		clusterUnstructured.SetUnstructuredContent(clusterMap)
-		// Sanitize credentials
-		sanitized, _ := clustermgr.SanitizeUnstructuredCluster(ctx, clusterUnstructured)
-		clusterObj := &cluster.Cluster{}
-		err = runtime.DefaultUnstructuredConverter.FromUnstructured(sanitized.Object, clusterObj)
-		if err != nil {
-			fmt.Printf("Error converting unstructured item to Cluster: %v\n", err)
-		}
-		sanitizedClusterList.Items = append(sanitizedClusterList.Items, *clusterObj)
-	}
-	return sanitizedClusterList, nil
-}
-
-// Get retrieves the object from the storage. It is required to support Patch.
-func (r *REST) Get(ctx context.Context, name string, options *metav1.GetOptions) (runtime.Object, error) {
-	sanitized := &unstructured.Unstructured{}
-	cluster, err := r.Store.Get(ctx, name, options)
-	if err != nil {
-		return sanitized, nil
-	}
-	clusterMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(cluster)
-	if err != nil {
-		return sanitized, nil
-	}
-	clusterUnstructured := &unstructured.Unstructured{}
-	clusterUnstructured.SetUnstructuredContent(clusterMap)
-	sanitized, _ = clustermgr.SanitizeUnstructuredCluster(ctx, clusterUnstructured)
-	return sanitized, nil
 }
