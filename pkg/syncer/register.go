713e6f27f97f0389b24e1b4a2ca073e5ef07950e
panshuai-ps <pjjwjw@163.com>
2023-12-29 16:11:50 +0800

    fix: license

--- pkg/syncer/register.go
+++ pkg/syncer/register.go
@@ -1,3 +1,17 @@
+// Copyright The Karbour Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package syncer
 
 import (
@@ -53,7 +67,7 @@ func createResources(ctx context.Context, client dynamic.Interface, mapper meta.
 		if len(bytes.TrimSpace(b)) == 0 {
 			continue
 		}
-		if err = createResource(ctx, client, mapper, data); err != nil {
+		if err = createResource(ctx, client, mapper, b); err != nil {
 			return fmt.Errorf("failed to create resource: %w", err)
 		}
 	}
