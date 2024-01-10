91aad2b50183c4fb5d0304fbecb6104a9355e0ab
elliotxx <951376975@qq.com>
2024-01-10 12:18:34 +0800

    fix: not robust enough audit func

--- pkg/infra/scanner/kubeaudit/kubeaudit.go
+++ pkg/infra/scanner/kubeaudit/kubeaudit.go
@@ -27,6 +27,7 @@ import (
 	"github.com/KusionStack/karbour/pkg/infra/scanner"
 	"github.com/KusionStack/karbour/pkg/infra/search/storage"
 	"github.com/KusionStack/karbour/pkg/util/cache"
+	"github.com/KusionStack/karbour/pkg/util/ctxutil"
 	"github.com/KusionStack/karbour/pkg/util/safeutil"
 	kubeauditpkg "github.com/elliotxx/kubeaudit"
 	"github.com/elliotxx/kubeaudit/auditors/all"
@@ -161,13 +162,17 @@ func (s *kubeauditScanner) scanManifest(ctx context.Context, noCache bool, resou
 }
 
 func (s *kubeauditScanner) scanManifestFor(ctx context.Context, resource *storage.Resource, manifest []byte) (scanner.ScanResult, error) {
-	report, err := s.kubeAuditor.AuditManifest("", bytes.NewBuffer(manifest))
+	// Extract the context and logger from the request.
+	log := ctxutil.GetLogger(ctx)
+
+	report, err := s.kubeAuditor.AuditSingleManifest(bytes.NewBuffer(manifest))
 	if err != nil {
 		return nil, err
 	}
 
 	results := report.RawResults()
 	if len(results) != 1 {
+		log.Info("invalid manifest", "manifest", string(manifest))
 		return nil, fmt.Errorf("the scan result number should be equal to 1")
 	}
 	result := results[0]
