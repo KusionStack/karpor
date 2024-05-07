9b150e84e0205032c9d5a8c1493ffc6223c9a183
hai-tian <tianhai_th@163.com>
2024-05-07 18:22:28 +0800

    chore: use new logo in ui

--- ui/src/pages/search/index.tsx
+++ ui/src/pages/search/index.tsx
@@ -20,7 +20,7 @@ import { DoubleLeftOutlined, DoubleRightOutlined } from '@ant-design/icons'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 import KarporTabs from '@/components/tabs/index'
-import logoJPG from '@/assets/karpor-full.png'
+import logoFull from '@/assets/img/logo-full.svg'
 import SqlSearch from '@/components/sqlSearch'
 import { defaultSqlExamples, tabsList } from '@/utils/constants'
 
@@ -97,7 +97,7 @@ const SearchPage = () => {
     <div className={styles.container} style={{ transform: `scale(${scale})` }}>
       <div className={styles.search}>
         <div className={styles.title}>
-          <img src={logoJPG} width="25%" alt="icon" />
+          <img src={logoFull} width="100%" alt="icon" />
         </div>
         <div className={styles.searchTab}>
           <KarporTabs
