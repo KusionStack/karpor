2b9331bd6234580adc1222f90d27d07757935191
ryanchia <iamryanchia@gmail.com>
2024-08-28 18:22:17 +0800

    style: refine license

--- pkg/syncer/jsonextracter/jsonpath.go
+++ pkg/syncer/jsonextracter/jsonpath.go
@@ -1,3 +1,4 @@
+// Copyright 2015 The Kubernetes Authors.
 // Copyright The Karpor Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// Copied and adapted from https://github.com/kubernetes/client-go/blob/master/util/jsonpath/jsonpath.go
 
 package jsonextracter
 
