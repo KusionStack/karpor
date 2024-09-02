85ea92364a76c2dd014ed3154eed86429a8fc628
Nate <wb-th358723@antgroup.com>
2024-09-02 15:43:48 +0800

    feat: search by natural langguage

--- ui/src/utils/request.ts
+++ ui/src/utils/request.ts
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setIsLogin } from '@/store/modules/globalSlice'
 
-export const HOST = 'https://30.177.33.254:7443'
+export const HOST = ''
 axios.defaults.baseURL = HOST
 
 axios.interceptors.request.use(
