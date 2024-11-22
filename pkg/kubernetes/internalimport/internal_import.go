34d7e200e631731e59be1633e551cf64fc1310df
elliotxx <951376975@qq.com>
2024-11-22 13:06:59 +0800

    chore(ci): optimize golangci-lint workflow config
    - Remove unnecessary cache configurations from golangci-lint-action
    - Remove redundant Go cache step from workflows
    - Sync golangci-lint configurations between check and release workflows

--- pkg/kubernetes/internalimport/internal_import.go
+++ pkg/kubernetes/internalimport/internal_import.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//nolint:all
 package internalimport
 
 // To use k8s.io/kubernetes as a library, it is required to import the relevant packages related to
