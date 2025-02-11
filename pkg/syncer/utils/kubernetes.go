e31181824b3f138dc69d81c6d9a3d347f66b5595
fanfan-yu <2228998656@qq.com>
2025-02-11 20:08:06 +0800

    fix: agent yaml

--- pkg/syncer/utils/kubernetes.go
+++ pkg/syncer/utils/kubernetes.go
@@ -1,3 +1,17 @@
+// Copyright The Karpor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package utils
 
 import (
@@ -17,8 +31,8 @@ import (
 	"github.com/KusionStack/karpor/config"
 )
 
+// ConvertToUnstructured converts the structured object to unstructured
 func ConvertToUnstructured(obj runtime.Object) (*unstructured.Unstructured, error) {
-	// Convert the structured object to unstructured
 	unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
 	if err != nil {
 		return nil, fmt.Errorf("failed to convert to Unstructured: %v", err)
@@ -29,7 +43,7 @@ func ConvertToUnstructured(obj runtime.Object) (*unstructured.Unstructured, erro
 	}, nil
 }
 
-// ApplyCrds apply crds to user cluster before other resources.
+// ApplyCrds applies crds to user cluster before other resources.
 func ApplyCrds(ctx context.Context, dynamicClient dynamic.Interface) error {
 	for _, crd := range config.CrdList {
 		var objMap map[string]interface{}
@@ -50,6 +64,7 @@ func ApplyCrds(ctx context.Context, dynamicClient dynamic.Interface) error {
 	return nil
 }
 
+// CreateOrUpdateUnstructured creates or updates object using dynamic client.
 func CreateOrUpdateUnstructured(ctx context.Context, dynamicClient dynamic.Interface, gvr schema.GroupVersionResource, namespace string, newObject *unstructured.Unstructured) error {
 	resourceClient := dynamicClient.Resource(gvr).Namespace(namespace)
 
